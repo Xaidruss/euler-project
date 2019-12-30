fe7b62008c16637e2d6e95b2313f68f4d17eaff6
Xaidruss <59364017+Xaidruss@users.noreply.github.com>
2019-12-30 14:02:17 +0000

    Solutions 15 to 16, 17 unfinished

--- 1 - 100/15.py
+++ 1 - 100/15.py
@@ -1,17 +1,28 @@
 # 1 x 1 => 2
 # 2 x 2 => 6
-# 3 x 3 => 84 
+# 3 x 3 => 20 
 # 
 # 
-# Write into r & d
-# for 3 x 3
-# Must have 3 r's 3 d's in any order
-# Can use combination formula
+# side length = x
+# each route required at least x * 2 sections
+# each route has x * 2 choices and can only be x long, we can use choose formula
+# 
+# x = 1
+# total jumps = 2
+# 
+# x = 2
+# total jumps = 4
+# 
+# x = 3
+# total jumps = 6
+# 
+# x = 20
+# total jumps = 40
 
 import math
 
 def c(total, choose):
-	return int((math.factorial(total) / math.factorial(total - choose))/math.factorial(choose))
+	return int((math.factorial(total) / math.factorial(total - choose)) / math.factorial(choose))
 
 num = 20
-print(c(num * num, num))
+print(c(num * 2, num))
